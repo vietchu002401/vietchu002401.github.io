8076d6ecffa552488a05f8e2971c41061a74f684
meocao <cuocsongmoihtt@gmail.com>
2025-03-19 21:41:19 +0700

    update

---
+++ script.js
@@ -0,0 +1,26 @@
+const kanaList = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ"];
+const latinList = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko"];
+const nameKana = document.getElementById("name-kana");
+const nameLatin = document.getElementById("name-latin");
+const btn = document.getElementById("next-btn");
+let index = 0;
+let previousIndex = 0;
+let randomNum = 0;
+
+const random = ()=>{
+    while(randomNum === index || randomNum === previousIndex){
+       randomNum = Math.floor(Math.random() * kanaList.length);
+    }
+    if (nameLatin.innerText !== "" || (nameLatin.innerText === "" && nameKana.innerText === "")){
+        previousIndex = index;
+        index = randomNum;
+        nameLatin.innerText = "";
+        nameKana.innerText = kanaList[index];
+        console.log(randomNum);
+        return;
+    }
+
+    nameLatin.innerText = latinList[index];
+}
+
+btn.addEventListener("click", random);
